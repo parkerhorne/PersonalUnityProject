ba1be17b0ff6fa1d348fe271be680a7270a23291
Parker Horne <parker.horne@digipen.edu>
2023-06-27 16:20:10 -0700

    reading data from json file

---
+++ PortfolioProject/Assets/Scripts/System/EnemyDataReader.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+[Serializable]
+public class EnemyInfo
+{
+    public string name;
+    public int cost;
+    public float health;
+    public float speed;
+}
+[System.Serializable]
+public class EnemyDataList
+{
+    public List<EnemyInfo> EnemyData;
+}
+public class EnemyDataReader : MonoBehaviour
+{
+    [SerializeField] public TextAsset jsonFile;
+
+    // Creating one static instance of the data reader
+    private static EnemyDataReader enemyDataReader;
+    public EnemyDataList enemyDataList;
+
+    private static EnemyDataReader Instance()
+    {
+        enemyDataReader = FindObjectOfType<EnemyDataReader>() as EnemyDataReader;
+        if (!enemyDataReader)
+        {
+            Debug.LogError("Json Reader is missing.");
+        }
+
+        return enemyDataReader;
+    }
+    void Start()
+    {
+        if (!jsonFile)
+        {
+            jsonFile = Resources.Load<TextAsset>("EnemyData");
+        }
+        enemyDataList = LoadDataFromJson();
+    }
+
+    EnemyDataList LoadDataFromJson()
+    {
+        Assert.IsNotNull(jsonFile);
+
+        EnemyDataList dataList = JsonUtility.FromJson<EnemyDataList>(jsonFile.text);
+
+        foreach (EnemyInfo info in dataList.EnemyData)
+        {
+            Debug.Log("Enemy found: " + info.name);
+        }
+        return dataList;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
